eac3c899e06a889630715e4c68e35c4f84cfd38c
nieukki <tomekpacek@o2.pl>
2025-12-14 10:38:35 +0100

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
 import GlobalErrorToasts from "@/components/GlobalErrorToasts";
 import { DialogsContextProvider } from "@/contexts/DialogsProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -38,6 +39,7 @@ export default function RootLayout({
           </main>
           <GlobalErrorToasts />
         </DialogsContextProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
